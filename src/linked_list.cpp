38e5d0f2598796b3e3aad54ae5de56da8c58693f
dgunther2001 <danielgunther2001@gmail.com>
2024-04-24 11:13:09 -0400

    Project structure setup

---
+++ src/linked_list.cpp
@@ -0,0 +1,1 @@
+#include "linked_list/linked_list.h"
---
+++ tests/test.cpp
@@ -0,0 +1,7 @@
+#include "linked_list/linked_list.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello linked list!!!\n";
+    return 0;
+}
